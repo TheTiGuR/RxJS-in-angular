6fbb31f8e51ddc845d1a77b08dd7a7011326c93b
TheTiGuR <thetigur@gmail.com>
2019-09-26 21:56:27 -0600

    First observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of, from} from 'rxjs'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+
+  ngOnInit(){
+    of(2, 4, 6, 8).subscribe(value => console.log(value));
+    from([2, 4, 6, 8]).subscribe(
+      item => {console.log(`resulting item...${item}`)},
+      err => {console.log(`error occurred...${err}`)},
+      () => {console.log(`All done`)}
+    )
+
+    const array = [
+      'Bob',
+      'Joe',
+      'Jim'
+    ]
+    from(array).subscribe(
+      item => {console.log(`Greetings, ${item}`)},
+      err => {console.log(`Dang, I got an error: ${err}`)},
+      () => {console.log('Done saying hello to everyone.')}
+    )
+  }
 }
